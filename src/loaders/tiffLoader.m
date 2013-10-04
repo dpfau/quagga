0f1f7b2dc6b7b53ec9c59ca90b2596c05cd338a0
David Pfau <david.pfau@gmail.com>
2013-10-04 17:32:12 -0400

    Changed name of sofroniewLoader to tiffLoader, as it is now a general-purpose script for loading multiple .tif files in a folder.

---
+++ src/loaders/tiffLoader.m
@@ -0,0 +1,22 @@
+function patch = tiffLoader(patchSz,xyzRng,tRng,dataPath,prefix)
+% tRng is superfluous here for the moment. 
+files = dir(dataPath);
+imgs = {};
+for i = 1:length(files)
+	if ~isempty(strfind(files(i).name,'.tif'))
+		if nargin < 5 || ~isempty(strfind(files(i).name,prefix))
+			info = imfinfo(fullfile(dataPath,files(i).name));
+			img = zeros([cellfun(@(x)diff(x)+1,xyzRng),length(info)]);
+			for j = 1:length(info)
+				img(:,:,j) = imread(fullfile(dataPath,files(i).name),'Index',j,'PixelRegion',xyzRng(1:2));
+	        end
+	        imgs{length(imgs)+1} = img;
+	    end
+	end
+end
+
+imgSz = size(imgs{1});
+patch = zeros([imgSz(1:end-1),0]);
+for i = 1:length(imgs)
+	patch = cat(ndims(imgs{i}),patch,imgs{i});
+end
