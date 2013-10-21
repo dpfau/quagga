be9b0087d6407a59aaa3b7f1c9a722601e55edc0
David Pfau <david.pfau@gmail.com>
2013-10-21 15:15:29 -0400

    Tweaks to printing in admmLasso

--- src/sparsity/admmLasso.m
+++ src/sparsity/admmLasso.m
@@ -1,15 +1,16 @@
-function x = admmLasso(A,b,l)
+function x = admmLasso(A,b,l,optose)
 % Implements ADMM for LASSO regression:
 % x = argmin_x l*||x||_1 + 1/2||Ax-b||^2_2
 % David Pfau, 2013
 
+if nargin < 4, optose = true; end
 [m,n] = size(A);
 assert(numel(b)==m)
 
 rho = 10*l;
 eps_rel = 1e-3;
 eps_abs = 1e-6;
-maxIter = 1000;
+maxIter = 500;
 
 shrinkage = @(x,t) sign(x).*max(abs(x)-t,0);
 
@@ -19,7 +20,7 @@ z = zeros(n,1);
 y = zeros(n,1);
 tic, X = eye(n)/rho - A'/(eye(m)+A*A'/rho)*A/rho^2; toc
 iter = 0;
-figure
+if optose, figure; end
 fprintf('Obj:\t\t r_p:\t\t e_p:\t\t r_d:\t\t e_d:\t\t nnz:\n')
 while (r_p > e_p || r_d > e_d) && iter < maxIter
     x  = X*(A'*b + rho*z - y);
@@ -35,7 +36,7 @@ while (r_p > e_p || r_d > e_d) && iter < maxIter
     
     fprintf( '%1.2e\t %1.2e\t %1.2e\t %1.2e\t %1.2e\t %d\n', ...
         l*sum(abs(x(:))) + 1/2*norm( A*x - b )^2, r_p, e_p, r_d, e_d, nnz(abs(x)>max(abs(x))/1e3) );
-    if mod(iter,10) == 0
+    if optose && mod(iter,10) == 0
         hold off, scatter(1:numel(x),x), hold on
         for i = 1:numel(x)
             line([i i],[0 x(i)]); 
