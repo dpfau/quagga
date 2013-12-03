5e403755d0a15c527bb36c36dc0d77f7141a77b0
freeman <the.freeman.lab@gmail.com>
2013-12-03 00:13:24 -0800

    dataPath not specified in calling function, hardcoding for now

--- src/loaders/ahrensLoader.m
+++ src/loaders/ahrensLoader.m
@@ -1,4 +1,6 @@
-function patch = ahrensLoader(patchSz,xyzRng,tRng,dataPath)
+function patch = ahrensLoader(patchSz,xyzRng,tRng)
+
+dataPath = '/groups/ahrens/ahrenslab/Misha/data_fish7_sharing_sample/data_for_sharing_01/12-10-05/Dre_L1_HuCGCaMP5_0_20121005_154312.corrected.processed';
 
 patch = zeros([cellfun(@(x)diff(x)+1,xyzRng),diff(tRng)+1]);
 nImg = (diff(tRng)+1)*(diff(xyzRng{3})+1);
